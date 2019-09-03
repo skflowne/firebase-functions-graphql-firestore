4a31cef2e4f95c28ba995d8bfe7501fdd3c52036
Geoffrey Hug <hug.geoffrey@gmail.com>
2019-09-03 05:46:48 +0200

    Fix dependencies, add example CRUD wishes, add example config files

--- functions/firebase/adminConfig.js
+++
@@ -1,5 +0,0 @@
-const adminConfig = {
-  serviceAccountId: "graphql-firestore-fb828@appspot.gserviceaccount.com"
-}
-
-module.exports = adminConfig
--- functions/firebase/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  apiKey: "AIzaSyBU-GfnaL0XPJjxQeVKN4VuURCt9dvjjms",
-  authDomain: "https://graphql-firestore-fb828.firebaseio.com",
-  projectId: "graphql-firestore-fb828"
-}
-
-module.exports = config
