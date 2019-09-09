79f0196a8d667bcb18b52febf4c93c6581e5d8c9
Geoffrey Hug <hug.geoffrey@gmail.com>
2019-09-09 10:22:42 +0200

    Actual config.example

--- functions/firebase/config.example.js
+++ functions/firebase/config.example.js
@@ -1,7 +1,7 @@
 const config = {
-    apiKey: "AIzaSyCfe4vBlIdTMn_ByhASsvLgGTfdMpmMkcg",
-    authDomain: "time-cost-calculator.firebaseapp.com",
-    projectId: "time-cost-calculator"
+    apiKey: "FIREBASE_API_KEY",
+    authDomain: "FIREBASE_PROJECT_AUTH_DOMAIN",
+    projectId: "FIREBASE_PROJECT_ID"
 }
 
 module.exports = config
